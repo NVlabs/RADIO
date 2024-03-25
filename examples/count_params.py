cf947a798d2c1fbe7aa79e11624e5130426165cd
Mike Ranzinger <mranzinger@nvidia.com>
2024-03-25 08:46:12 -0700

    count_params.py cleanup

--- examples/count_params.py
+++ examples/count_params.py
@@ -5,49 +5,28 @@
 # and any modifications thereto.  Any use, reproduction, disclosure or
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
-import argparse
-from collections import defaultdict
-from functools import partial
-import gc
-import math
-import os
-from PIL import Image
-import random
 import time
 from tqdm import tqdm
-from typing import Any, Dict, Iterable, List, Tuple
 
-import cv2
-import numpy as np
 import torch
-from torch import nn
-import torch.distributed as dist
-from torch.utils.data import DataLoader
-import torch.nn.functional as F
-from torchvision.utils import make_grid
-from torchvision.datasets import ImageFolder
 
-from einops import rearrange
 
-from datasets import load_dataset_builder, load_dataset
-from datasets.distributed import split_dataset_by_node
-
-from common import rank_print, load_model, get_standard_transform, collate
+from common import load_model
 
 MODELS = [
-    # ('OpenAI CLIP', 'open_clip,ViT-L-14-336,openai', 336, 16),
-    # ('OpenCLIP', 'open_clip,ViT-H-14,laion2b_s32b_b79k', 224, 16),
-    # ('DFN CLIP', 'open_clip,ViT-H-14-378-quickgelu,dfn5b', 378, 16),
-    # ('SigLIP', 'open_clip,ViT-SO400M-14-SigLIP-384,webli', 384, 16),
-    # ('MetaCLIP', 'open_clip,ViT-H-14-quickgelu,metaclip_fullcc', 224, 16),
+    ('OpenAI CLIP', 'open_clip,ViT-L-14-336,openai', 336, 16),
+    ('OpenCLIP', 'open_clip,ViT-H-14,laion2b_s32b_b79k', 224, 16),
+    ('DFN CLIP', 'open_clip,ViT-H-14-378-quickgelu,dfn5b', 378, 16),
+    ('SigLIP', 'open_clip,ViT-SO400M-14-SigLIP-384,webli', 384, 16),
+    ('MetaCLIP', 'open_clip,ViT-H-14-quickgelu,metaclip_fullcc', 224, 16),
 
-    # ('DINOv2-g-reg', 'dinov2_vitg14', 224, 16),
+    ('DINOv2-g-reg', 'dinov2_vitg14', 224, 16),
 
-    # ('SAM-H', 'sam,/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/model_zoo/sam/sam_vit_h_4b8939.pth', 1024, 4),
+    ('SAM-H', 'sam,/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/model_zoo/sam/sam_vit_h_4b8939.pth', 1024, 4),
 
-    # ('RADIO', 'radio_v2.1', 432, 16),
+    ('RADIO', 'radio_v2.1', 432, 16),
 
-    # ('InternViT-6b-224', 'InternViT-6B-224px', 224, 8),
+    ('InternViT-6b-224', 'InternViT-6B-224px', 224, 8),
     ('InternViT-6B-448-1.2', 'InternViT-6B-448px-V1-2', 448, 8),
 ]
 
@@ -80,38 +59,39 @@ def main(rank: int = 0, world_size: int = 1):
         print(f'Num Params: {num_params_m:.1f}M')
 
         print(f'Calculating throughput...')
-        model.cuda().eval().float()
+        model.cuda().eval()
         # preproc.cuda().eval()
         buff = torch.empty(batch_size, 3, resolution, resolution, dtype=torch.float32, device='cuda')
-        # buff = preproc(buff)
+        pt_buff = preproc(buff)
         throughput = 0
-        # with torch.autocast('cuda', dtype=torch.bfloat16):
-        #     # First one is free
-        #     model(buff)
-        #     torch.cuda.synchronize()
-        #     start_time = time.time()
-        #     NUM_BATCHES = 100
-        #     for _ in tqdm(range(NUM_BATCHES)):
-        #         model(buff)
-        #     torch.cuda.synchronize()
-        #     end_time = time.time()
+        with torch.autocast('cuda', dtype=torch.bfloat16):
+            # First one is free
+            model(pt_buff)
+            torch.cuda.synchronize()
+            start_time = time.time()
+            NUM_BATCHES = 100
+            for _ in tqdm(range(NUM_BATCHES)):
+                model(pt_buff)
+            torch.cuda.synchronize()
+            end_time = time.time()
 
-        # throughput = (NUM_BATCHES * buff.shape[0]) / (end_time - start_time)
-        # print(f'Done. {throughput:.2f} im/sec')
+        throughput = (NUM_BATCHES * buff.shape[0]) / (end_time - start_time)
+        print(f'Done. {throughput:.2f} im/sec')
 
-        onnx_file_path = '/tmp/trt_model.onnx'
-        torch.onnx.export(
-            model,
-            buff,
-            onnx_file_path,
-            input_names=['input'],
-            output_names=['output'],
-            export_params=True,
-            opset_version=19,
-        )
-        # os.system(f'onnxsim {onnx_file_path} {onnx_file_path}')
-        os.system(f'trtexec --onnx={onnx_file_path} --fp16 --allowGPUFallback --workspace=300000000')
-        print('\n\n\n\n\n')
+        # # Uncomment to profile model with TensorRT
+        # model.float()
+        # onnx_file_path = '/tmp/trt_model.onnx'
+        # torch.onnx.export(
+        #     model,
+        #     buff,
+        #     onnx_file_path,
+        #     input_names=['input'],
+        #     output_names=['output'],
+        #     export_params=True,
+        #     opset_version=19,
+        # )
+        # os.system(f'trtexec --onnx={onnx_file_path} --fp16 --allowGPUFallback --workspace=300000000')
+        # print('\n\n\n\n\n')
 
         prms.append((name, num_params_m, throughput))
 
