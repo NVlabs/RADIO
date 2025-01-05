f304b7e37056ca69f42ed9de6bc4ae06297770ba
Mike Ranzinger <mranzinger@nvidia.com>
2025-01-04 18:32:14 -0800

    More robust mode switching

--- tools/im_join.py
+++ tools/im_join.py
@@ -11,6 +11,7 @@ import os
 
 import cv2
 import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 
 
 def main():
@@ -20,8 +21,20 @@ def main():
     parser.add_argument('-c', '--cols', type=int, default=0, help='The number of columns in the mosaic')
     parser.add_argument('--header', type=int, default=128, help='The height for the cell headers')
     parser.add_argument('--header-color', type=int, default=255, help='The color of the header')
+    parser.add_argument('--header-text', type=str, default='', help='Specify the text to be rendered into the header')
 
     args = parser.parse_args()
+
+    header_lookup = dict()
+
+    header_parts = args.header_text.split(';')
+    for part in header_parts:
+        if not part:
+            continue
+        row, col, text = part.split(',')
+        row = int(row)
+        col = int(col)
+        header_lookup[(row, col)] = text
 
     all_files = []
     for indir in args.input_dirs:
@@ -39,16 +52,23 @@ def main():
         num_cols = int(math.ceil(math.sqrt(len(args.input_dirs))))
     num_rows = int(math.ceil(len(args.input_dirs) / num_cols))
 
+    # Load a font (default PIL font if none available)
+    try:
+        # Use a TTF font available on your system
+        font = ImageFont.truetype("OpenSans-VariableFont_wdth,wght.ttf", max(8, args.header - 8))
+    except IOError:
+        font = ImageFont.load_default()  # Default font if TTF font isn't available
+
     for group in all_files:
         fname = os.path.basename(group[0])
 
         imgs = [cv2.imread(f) for f in group]
 
-        max_width = 0
-        max_height = 0
-        for im in imgs:
-            max_width = max(max_width, im.shape[1])
-            max_height = max(max_height, im.shape[0])
+        max_width = imgs[0].shape[1]
+        max_height = imgs[0].shape[0]
+        # for im in imgs:
+        #     max_width = max(max_width, im.shape[1])
+        #     max_height = max(max_height, im.shape[0])
 
         for i in range(len(imgs)):
             im = imgs[i]
@@ -63,7 +83,7 @@ def main():
                     scale = max_width / im.shape[1]
                 rs_height = int(round(im.shape[0] * scale))
                 rs_width = int(round(im.shape[1] * scale))
-                im = cv2.resize(im, dsize=(rs_width, rs_height), interpolation=cv2.INTER_CUBIC)
+                im = cv2.resize(im, dsize=(rs_width, rs_height), interpolation=cv2.INTER_NEAREST)
 
                 if im.shape[0] != max_height or im.shape[1] != max_width:
                     le_width = (max_width - im.shape[1]) // 2
@@ -75,6 +95,9 @@ def main():
 
         buff = np.full((num_rows * (max_height + args.header) + (2 * (num_rows - 1)), num_cols * max_width + (2 * (num_cols - 1)), 3), 255, dtype=imgs[0].dtype)
 
+        pil_buff = Image.fromarray(buff, mode='RGB')
+        draw = ImageDraw.Draw(pil_buff)
+
         for i, img in enumerate(imgs):
             c = i % num_cols
             r = i // num_cols
@@ -83,6 +106,11 @@ def main():
             r = r * (max_height + 2 + args.header)
 
             buff[r:r+args.header, c:c+max_width] = args.header_color
+
+            header_text = header_lookup.get((r, c), None)
+            if header_text:
+                # bbox = draw.textbbox((4, 4), header_text, font)
+                draw.text((4, 4), header_text, font=font)
 
             r += args.header
 
