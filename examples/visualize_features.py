Getting it working in the examples. Fix a bug with uniform r calculation when final_r is extreme.

examples/visualize_features.py
@@ -110,6 +110,8 @@ def main(rank: int = 0, world_size: int = 1):
                         choices=['sparse', 'dense'])
     parser.add_argument('--interpolation', default='bilinear', type=str, help='Interpolation mode')
     parser.add_argument('--skip', default=0, type=int, help='Skip the first N components')
+    parser.add_argument('--tome-config', type=str, default=None,
+                    help='Configure the model with ToMe')
 
     args, _ = parser.parse_known_args()
 
@@ -126,8 +128,11 @@ def main(rank: int = 0, world_size: int = 1):
                 print(f'Using "{chk_path}" as model version.')
                 break
 
+    if args.tome_config and 'expand' not in args.tome_config:
+            args.tome_config += ',expand=1'
+
     model, preprocessor, info = load_model(args.model_version, vitdet_window_size=args.vitdet_window_size, adaptor_names=args.adaptor_name,
-                                           torchhub_repo=args.torchhub_repo)
+                                           torchhub_repo=args.torchhub_repo, tome_config=args.tome_config)
     model.to(device=device).eval()
     if isinstance(preprocessor, nn.Module):
         preprocessor.to(device).eval()

examples/zero_shot_imagenet.py
@@ -168,9 +168,13 @@ def main(rank: int = 0, world_size: int = 1):
     start_time = torch.cuda.Event(enable_timing=True)
     end_time = torch.cuda.Event(enable_timing=True)
     postproc = getattr(model, 'zero_shot_postproc', lambda x: x)
+
+    total_tokens = torch.tensor(0, device=device, dtype=torch.float64)
     with torch.inference_mode(), tqdm(total=num_examples, disable=rank > 0) as t:
         for i, batches in enumerate(loader):
             if i == 1:
+                if world_size > 1:
+                    dist.barrier()
                 start_time.record()
             for images, targets in batches:
                 images = images.to(device=device, non_blocking=True)
@@ -178,8 +182,12 @@ def main(rank: int = 0, world_size: int = 1):
 
                 with torch.autocast(device.type, dtype=torch.bfloat16, enabled=args.amp):
                     output = model(images)
-                    summary = output[args.adaptor_name].summary
+                    ada_output = output[args.adaptor_name]
+                    summary = ada_output.summary
                     summary = F.normalize(summary, dim=-1)
+
+                    features = ada_output.features
+                    total_tokens += features.shape[0] * features.shape[1]
 
                     logits = summary.to(classifier.dtype) @ classifier
                     logits = postproc(logits)
@@ -196,13 +204,18 @@ def main(rank: int = 0, world_size: int = 1):
         rank_print('\tWaiting for all ranks to complete...')
         num_processed = torch.tensor(num_processed, device=device)
         dist.reduce(num_processed, dst=0, op=dist.ReduceOp.SUM)
+        dist.reduce(total_tokens, dst=0, op=dist.ReduceOp.SUM)
 
         for k, acc in topks.items():
             dist.reduce(acc, dst=0, op=dist.ReduceOp.SUM)
         rank_print('\tDone')
     rank_print('Done')
 
+    avg_tokens = (total_tokens / num_processed).item()
+    rank_print(f'Average Tokens per Image: {avg_tokens:.2f}')
+
     end_time.record()
+    end_time.synchronize()
     elapsed_time_sec = start_time.elapsed_time(end_time) / 1000.0
 
     rank_print(f'Resolution: {args.resolution}')
@@ -216,7 +229,7 @@ def main(rank: int = 0, world_size: int = 1):
             with open(args.csv_out, 'a') as fd:
                 if args.csv_exp_name:
                     fd.write(f'{args.csv_exp_name},')
-                fd.write(f'{" ".join(str(r) for r in args.resolution)},{acc:.4f},{elapsed_time_sec:.3f}\n')
+                fd.write(f'{" ".join(str(r) for r in args.resolution)},{acc:.4f},{elapsed_time_sec:.3f},{avg_tokens:.3f}\n')
                 # drop_span = args.drop_span
                 # if not drop_span:
                 #     drop_span = ['baseline', '']

radio/tome/timm.py
@@ -132,22 +132,58 @@ def _tx_pre_hook(model: VisionTransformer, mode: ToMeMode | str = ToMeMode.DISAB
         """
         model._tome_info["num_input_tokens"] = num_tokens = input[0].shape[1]
 
-        def _get_r(r_pct: float | None = None, final_r: int | None = None, per_r: int | None = None):
+        def _get_r(r_pct: float | None = None, final_r: int | None = None, per_r: int | None = None, num_final: int | None = None):
             r = None
+            num_tokens = input[0].shape[1] - model._tome_info['class_token']
             if r_pct is not None:
-                num_tokens = input[0].shape[1] - model._tome_info['class_token']
                 final_r = int(r_pct * num_tokens)
+
+            if num_final is not None:
+                final_r = num_tokens - num_final
 
             if final_r is not None:
                 all_r = [0]
+                tail = [int(num_tokens - final_r)]
+                for _ in range(len(model.blocks) - 1):
+                    tail.append(tail[-1] * 2)
+                tail.reverse()
+
                 fp_per_r = final_r / (len(model.blocks) - 1)
+
+                leftover = 0
+                first_lo_idx = len(model.blocks) - 1
                 for i in range(1, len(model.blocks)):
                     fp_curr = int(i * fp_per_r)
                     fp_prev = int((i - 1) * fp_per_r)
-                    all_r.append(fp_curr - fp_prev)
-                calc_r = sum(all_r)
-                if calc_r < final_r:
-                    all_r[-1] += final_r - calc_r
+
+                    num_curr = fp_curr - fp_prev
+                    num_possible = tail[i]
+                    if num_curr > num_possible:
+                        curr_leftover = num_curr - num_possible
+                        leftover += curr_leftover
+                        num_curr = num_possible
+                        first_lo_idx = min(first_lo_idx, i)
+                    all_r.append(num_curr)
+
+                placed = True
+                while leftover > 0 and placed:
+                    fp_per_r = leftover / (first_lo_idx - 1)
+                    new_leftover = 0
+                    for i in range(1, first_lo_idx):
+                        fp_curr = int(i * fp_per_r)
+                        fp_prev = int((i - 1) * fp_per_r)
+
+                        num_curr = fp_curr - fp_prev
+                        num_possible = tail[i] - all_r[i]
+                        if num_curr > num_possible:
+                            curr_leftover = num_curr - num_possible
+                            new_leftover += curr_leftover
+                            num_curr = num_possible
+                            first_lo_idx = min(first_lo_idx, i)
+                        all_r[i] += num_curr
+                    placed = new_leftover < leftover
+                    leftover = new_leftover
+
                 r = all_r
 
             if r is None:
@@ -163,7 +199,8 @@ def _tx_pre_hook(model: VisionTransformer, mode: ToMeMode | str = ToMeMode.DISAB
             r_pct = mode_args.get("r_pct", None)
             final_r = mode_args.get("final_r", None)
             per_r = mode_args.get("r", None)
-            r = _get_r(r_pct=r_pct, final_r=final_r, per_r=per_r)
+            num_final = mode_args.get("num_final", None)
+            r = _get_r(r_pct=r_pct, final_r=final_r, per_r=per_r, num_final=num_final)
         elif mode == ToMeMode.DYNAMIC:
             with torch.no_grad():
                 pred_r: torch.Tensor = model.r_predictor(model._tome_info["input_img"])
@@ -176,8 +213,8 @@ def _tx_pre_hook(model: VisionTransformer, mode: ToMeMode | str = ToMeMode.DISAB
             else:
                 pred_r = pred_r[0]
 
-            final_r = (pred_r < loss_threshold).sum().item()
-            r = _get_r(final_r=final_r)
+            r_pct = min((pred_r < loss_threshold).sum().item() / 100, 0.95)
+            r = _get_r(r_pct=r_pct)
         else:
             raise ValueError(f"Unknown ToMe mode {mode}.")
 
@@ -193,8 +230,7 @@ def _input_pre_hook(model: VisionTransformer):
         """
         Forward pre-hook to initialize ToMe info before processing patches.
         """
-        if model.training:
-            model._tome_info["input_img"] = input[0]
+        model._tome_info["input_img"] = input[0]
 
     return patches_forward_pre_hook
 
@@ -313,7 +349,7 @@ def apply_patch(
         "prop_attn": prop_attn,
         "class_token": num_prefix,
         "distill_token": False,
-        "expand": mode_args.get("expand", False),
+        "expand": bool(mode_args.get("expand", False)),
         "rng": rng,
     }
 